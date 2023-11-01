6b18c67e2fe276f9f0eefc7b8042c3f65fb8d3d9
jenna summerday <hello@salonaissistant.com>
2023-11-01 07:36:54 +0000

    Update UploadcareComponent.tsx

--- plugin/src/UploadcareComponent.tsx
+++ plugin/src/UploadcareComponent.tsx
@@ -6,7 +6,20 @@ LR.registerBlocks(LR);
 export const UploadcareComponent: React.FC = () => {
   return (
     <div>
-      {/* Your Uploadcare widget will go here */}
+      <lr-config
+        ctx-name="my-uploader"
+        pubkey="9a23719f8b1f058adbac"
+        maxLocalFileSizeBytes={10000000}
+        multiple={false}
+        imgOnly={true}
+        sourceList="local, url, camera, facebook, gphotos, instagram"
+      ></lr-config>
+      <lr-file-uploader-regular
+        css-src="https://cdn.jsdelivr.net/npm/@uploadcare/blocks@0.25.0/web/lr-file-uploader-regular.min.css"
+        ctx-name="my-uploader"
+        className="my-config"
+      >
+      </lr-file-uploader-regular>
     </div>
   );
 };
